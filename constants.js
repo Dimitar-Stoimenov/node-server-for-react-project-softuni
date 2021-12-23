bd99e313186de66ccf23ee8cd79bdcb4df332263
Dimitar Stoimenov <dstoimen0@gmail.com>
2021-12-23 04:58:22 +0200

    asd

---
+++ constants.js
@@ -0,0 +1,4 @@
+exports.PORT = process.env.PORT || 3030;
+exports.DB_CONNECTION_STRING = 'mongodb+srv://admin:e3QwnHo9Ee4IjDy7@test.cfkm1.mongodb.net/react-project?retryWrites=true&w=majority';
+exports.SECRET = '2156NU2UN7V79ANJS9X';
+// exports.AUTH_COOKIE_NAME = 'token';
